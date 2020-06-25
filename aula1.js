41f8f72cd3d8f6333499c9055284a3f4e98049bb
Nathalia Rodrigues da Cruz <66573618+Nathalia11@users.noreply.github.com>
2020-06-25 12:07:52 -0300

    Update and rename index.html.js to aula1.js

---
+++ aula1.js
@@ -0,0 +1,35 @@
+//index.html
+
+<!DOCTYPE html>
+<html lang="pt br">
+    <head>
+        <title>Minha página</title>
+        <meta charset="utf 8">
+        <script type="text/javascript" src="js/main.js"></script>
+     <head>
+     <body>
+                <h1>Minha página</h1>
+     </body>     
+</html>   
+
+
+//Criar uma pasta chamada js e dentro dela um arquico chamado main.js
+
+var nome = "Nathalia Rodrgues";
+var idade = 24;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+alert(nome + "tem" + idade + "anos");
+alert(idade + idade2) //ele vai somar as duas idades
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());//Deixa tudo em maiusculo
+console.log(frase.toLocaleLowerCase());//Deixa tudo em minusculo
+
+
+
+
