f446a83cd0f8f47a5c156faf44715268259d9899
Nathalia Rodrigues da Cruz <66573618+Nathalia11@users.noreply.github.com>
2020-06-25 12:34:38 -0300

    Create aula2.js

---
+++ aula2.js
@@ -0,0 +1,21 @@
+Array e Dicionário
+
+
+var lista = ["maça", "pêra", "laranja"];
+lista.push("uva")//adiciona um elemento
+lista.pop();//retira o ultimo elemento
+console.log(lista.length);// ver o tamnho da minha lista
+console.log(lista.reverse());//troca a ordem da lista, de traz para frente
+console.log(lista.toString()); //traz a lista como uma string
+console.log(lista.join(" - "));//a lista fica separada por traço
+
+
+
+//Dicionário
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+
+
+//lista de dicionários
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
